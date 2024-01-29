d08702df7f6ea1e1513f01da4cce2bf671775ec8
umidjonxikmatov <xikmatovumidjon1120@gmail.com>
2024-01-29 23:51:51 +0500

    local

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,19 @@ from pages.forms import ContactModelForms
 from pages.models import AboutModel, ImagesModel, ServiceModel, BlogModel, ContactModel
 
 
+def home_page_view(request):
+    about = AboutModel.objects.all().order_by('-created_at')[:3]
+    image = ImagesModel.objects.all().order_by('-created_at')[:6]
+    services = ServiceModel.objects.all().order_by('-created_at')[:6]
+    blogs = BlogModel.objects.all().order_by('-created_at')[:3]
+    context = {
+        "abouts": about,
+        "images": image,
+        "services": services,
+        "blogs": blogs,
 
+    }
+    return render(request, template_name='home.html', context=context)
 
 
 def contact_page_view(request):
